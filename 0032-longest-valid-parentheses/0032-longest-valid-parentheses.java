9419e5d787a4d12219a7be47eca45ae8e8ad5dc8
aadyanair <149501018+aadyanair@users.noreply.github.com>
2025-04-16 18:05:16 +0530

    Time: 3 ms (77.2%), Space: 43 MB (68.18%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxl = 0;
+        if(s.length()==0) return 0;
+        Stack<Integer> st = new Stack<>();
+        st.push(-1); //to handle base condition like starting is ) bracket
+        for(int i=0; i<s.length();i++){
+            if(s.charAt(i)=='(') st.push(i);
+            else{
+                st.pop();
+                if(st.isEmpty()) st.push(i);
+                maxl = Math.max(maxl,i-st.peek());
+            }
+        }
+        return maxl;
+    }
+}

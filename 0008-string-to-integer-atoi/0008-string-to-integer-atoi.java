6199ebd91bcce270efc1f5be4039926dfa6e3535
aadyanair <149501018+aadyanair@users.noreply.github.com>
2025-05-30 09:36:55 +0530

    Time: 1 ms (100%), Space: 42.3 MB (90.99%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,29 +1,32 @@
 class Solution {
     public int myAtoi(String s) {
         s = s.trim();
-        int sign=1, i=0, res=0;
 
         if(s.length()==0) return 0;
+        int sign =1, res=0, i=0;
 
-        if(s.charAt(0)=='-') {
+        if(s.charAt(0)=='-'){
             sign=-1;
             i++;
-        } else if(s.charAt(0)=='+'){
+        }else if(s.charAt(0)=='+'){
             i++;
         }
 
         while(i<s.length()){
             char c = s.charAt(i);
+
             if(c<'0' || c>'9') break;
-
             int digit = c-'0';
-            if(res > (Integer.MAX_VALUE - digit)/10){
-                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-            res = res * 10 + (c-'0');
 
+            if(res > (Integer.MAX_VALUE-digit)/10){
+                return (sign==1)?Integer.MAX_VALUE:Integer.MIN_VALUE;
+
+            }
+
+            res = res*10+digit;
             i++;
         }
         return (int) (sign*res);
+        
     }
 }

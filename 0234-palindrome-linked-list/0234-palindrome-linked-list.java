a4ac6d039758fd1af111a280e348eda8b468776e
aadyanair <149501018+aadyanair@users.noreply.github.com>
2025-06-23 16:09:36 +0530

    Time: 5 ms (55.59%), Space: 69 MB (16.32%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,31 +9,37 @@
  * }
  */
 class Solution {
+
     ListNode rev(ListNode head){
-        ListNode  cur = head;
-        ListNode  prev = null;
+        ListNode cur = head;
+        ListNode prev = null;
         ListNode next = null;
+
         while(cur!=null){
             next = cur.next;
             cur.next = prev;
             prev = cur;
             cur = next;
-        }
-        return prev;
+        }return prev;
     }
+
+
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head, fast = head;
-        while(fast.next!=null && fast.next.next!=null){
+        ListNode slow =head,fast=head;
+        while(fast.next!=null && fast.next.next!=null ){
             slow = slow.next;
             fast = fast.next.next;
+
         }
+
         ListNode secondHalf = slow.next;
-        slow.next = null; //breaking the connection
+        slow.next = null;
         secondHalf = rev(secondHalf);
         while(secondHalf!=null){
             if(head.val!=secondHalf.val){
                 return false;
             }
+
             head = head.next;
             secondHalf = secondHalf.next;
         }

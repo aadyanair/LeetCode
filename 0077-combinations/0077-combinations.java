797a546f40213f19c07bfd7707519ffc05a66554
aadyanair <149501018+aadyanair@users.noreply.github.com>
2025-06-29 14:29:23 +0530

    Time: 18 ms (79.89%), Space: 94.8 MB (42.31%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,22 +1,21 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Solution {
+class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), 1, n, k);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        combinations(res,list,1,n,k);
+        return res;
     }
-    
-    private void backtrack(List<List<Integer>> result, List<Integer> temp, int start, int n, int k) {
-        if (temp.size() == k) {
-            result.add(new ArrayList<>(temp));
+
+    public void combinations(List<List<Integer>> res, List<Integer> list, int start, int n, int k){
+        if(list.size()==k){
+            res.add(new ArrayList<>(list));
             return;
         }
-        for (int i = start; i <= n; i++) {
-            temp.add(i);
-            backtrack(result, temp, i + 1, n, k);
-            temp.remove(temp.size() - 1);
+
+        for(int i=start;i<=n;i++){
+            list.add(i);
+            combinations(res,list,i+1,n,k);
+            list.remove(list.size()-1);
         }
     }
 }
